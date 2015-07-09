cd591feb7f1f8c6770cea1504859a616bf12365a
williamwun <u201120911@upc.edu.pe>
2015-07-09 16:28:12 -0500

    william wun

--- app/src/main/java/upc/com/pe/trabajo/models/ComputersAdapter.java
+++ app/src/main/java/upc/com/pe/trabajo/models/ComputersAdapter.java
@@ -1,5 +1,20 @@
 package upc.com.pe.trabajo.models;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import upc.com.pe.trabajo.R;
+//import upc.com.pe.trabajo.activities.Co;
 /**
  * Created by William on 09/07/2015.
  */
