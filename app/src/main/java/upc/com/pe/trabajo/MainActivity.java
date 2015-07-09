b61736abc73a37e100d2be1f1109cc05cd399cfe
williamwun <u201120911@upc.edu.pe>
2015-07-09 03:16:16 -0500

    first

--- app/src/main/java/upc/com/pe/trabajo/MainActivity.java
+++ app/src/main/java/upc/com/pe/trabajo/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
+//comment
 public class MainActivity extends ActionBarActivity {
 
     Button loginButton;
